4c98cd8f4ee97338e29bee9069fa1726ec878ad3
Akhil Mishra <akhilmishra184@gmail.com>
2023-08-22 21:18:59 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import ReactGridLayout from './Components/ReactGridLayout';
 
-function App() {
-
+const  App=()=> {
+  
   let attributeValues={
     NumberOfColumns:3,
     NumberOfBoxes:36
-
   }
+
   return (
     <div className="App">
       <ReactGridLayout attributeValues={attributeValues}/>
